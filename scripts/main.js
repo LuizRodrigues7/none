8eaaf985f3a4fc9c935f4ef69296ebdc49023117
LuizRodrigues7 <luiz.rsneto7@gmail.com>
2024-11-30 20:27:48 -0300

    att
    finalizei o site para o pai da minha namorada.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-const = 
+const caixa1 = document.getElementById('box01')
+const caixa2 = document.getElementById('box02')
+const quest = document.getElementById('question')
+
+function RevealImages(){
+    if (caixa1.style.display = "none"){
+        caixa1.style.display = "flex";
+        caixa2.style.display = "flex";
+        quest.style.display = "grid";
+    }
+}
